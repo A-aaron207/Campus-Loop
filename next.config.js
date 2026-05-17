10b132080404a9a6c9f1509d7089bc3b2d9eae90
Aaron Saha <aaronsaha@pop-os.lan>
2026-05-17 08:19:10 +0530

    build: standardize module type and clean next config

--- next.config.js
+++ next.config.js
@@ -4,13 +4,7 @@ const nextConfig = {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
     // your project has type errors.
-    // !! WARN !!
     ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
   },
   // This is the key for full-stack apps on Vercel
   // It disables static prerendering for pages that need the database
